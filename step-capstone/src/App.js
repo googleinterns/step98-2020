a58c641d2f963f4ce5e916a659e9a20849fceb9b
Zach Irving <zirving@lion.lmu.edu>
2020-06-17 17:18:48 -0400

    working on connecting with gcloud appengine for deployment

--- step-capstone/src/App.js
+++ step-capstone/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  render () {
+    return(
     <div className="App">
-      
+      <h1> Hello World </h1>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
