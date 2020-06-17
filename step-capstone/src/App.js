d7ca82be7b619f6ab88bee49adcc040db16200ba
Emmie Ohnuki <eohnuki@eohnuki-macbookpro.roam.corp.google.com>
2020-06-17 11:04:46 -0400

    Install material-ui into project

--- step-capstone/src/App.js
+++ step-capstone/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TravelObject from './Components/TravelObject'
 
 function App() {
   return (
     <div className="App">
-
+      
     </div>
   );
 }
