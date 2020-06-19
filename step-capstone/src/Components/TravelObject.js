09b82fad30e23d9d8964a27e416dbe3902d28f22
Emmie Ohnuki <eohnuki@eohnuki-macbookpro.roam.corp.google.com>
2020-06-19 12:41:49 -0400

    Organize components into grid and use material design icons.

--- step-capstone/src/Components/TravelObject.js
+++ step-capstone/src/Components/TravelObject.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Container, Grid, Button} from '@material-ui/core'
+import { Grid, Button, Typography} from '@material-ui/core'
+import { Edit, Delete} from '@material-ui/icons'
 
 export default function TravelObject(props) {
     let content = null;
     switch(props.type) {
         case 'event':
-            content = <div>Event!</div>;
+            content = <div>Event!</div>
             break;
         case 'flight':
-            content = <h1>Flight!</h1>
+            content = <Typography variant="h4" gutterBottom>Flight!</Typography>
             break;
         case 'hotel':
             content = <div>Hotel!</div>
@@ -18,11 +19,21 @@ export default function TravelObject(props) {
             break;
     }
     return (
-        <div>
-            {content}
-            <Button onClick={() => console.log("editing")}>Edit</Button>
-            <Button onClick={() => console.log("deleting")}>Delete</Button>
-        </div>
+        <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item>
+                {content}
+            </Grid>
+            <Grid item>
+                <Grid container direction="column">
+                    <Edit 
+                        onClick={() => console.log("editing")}
+                    />
+                    <Delete 
+                        onClick={() => console.log("deleting")}
+                    />
+                </Grid>
+            </Grid>
+        </Grid>
     )
     
 }
