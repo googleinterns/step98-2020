a70cfc793fd622c810796b9d05a947f1d2f8c292
Emmie Ohnuki <emaohn@gmail.com>
2020-06-22 17:29:39 -0400

    Fix grid spacing

--- step-capstone/src/components/TravelObject.js
+++ step-capstone/src/components/TravelObject.js
@@ -10,7 +10,7 @@ export default function TravelObject(props) {
             content = <Typography variant="h4" gutterBottom>Event!</Typography>
             break;
         case 'flight':
-            content = <Flight data={ props.data }/>
+            content = <Flight data={props.data} />
             break;
         case 'hotel':
             content = <Typography variant="h4" gutterBottom>Hotel!</Typography>
@@ -22,9 +22,11 @@ export default function TravelObject(props) {
         <Card>
             <CardContent>
                 <Grid container direction="row" justify="flex-end" alignItems="center">
-                    <Box mr={10} width="100%" height="100%">
-                        {content}
-                    </Box>
+                    <Grid item xs>
+                        <Box mr={10} width="100%" height="100%">
+                            {content}
+                        </Box>
+                    </Grid>
                     <Grid item>
                         <Grid container direction="column">
                             <Edit
