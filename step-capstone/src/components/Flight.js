20a22f332db192b9989c5f57ceb5c41c76c75c2f
Emmie Ohnuki <emaohn@gmail.com>
2020-06-22 17:21:36 -0400

    Render flight data

--- step-capstone/src/components/Flight.js
+++ step-capstone/src/components/Flight.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Box, Grid, Typography } from '@material-ui/core'
 
-export default function Flight(props) {
+export default function Flight({ data }) {
     return(
-        <Typography variant="h5" gutterBottom>{ props.data.departureAirport } to { props.data.arrivalAirport }</Typography>
+        <Grid item>
+            <Typography variant="h6" gutterBottom>Flight from { data.departureAirport } to { data.arrivalAirport }</Typography>
+            <Typography variant="subtitle2" gutterBottom>Departure: { data.departureDate }</Typography>
+            <Typography variant="subtitle2" gutterBottom>Arrival: { data.arrivalDate }</Typography>
+            <Typography variant="body2" gutterBottom>{ data.description }</Typography>
+        </Grid>
     )
 }
