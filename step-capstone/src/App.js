ab581f23f246aa856d47170cde42aa3ba9b4008f
Emmie Ohnuki <emaohn@gmail.com>
2020-06-22 21:36:43 -0400

    Add function to fetch data and fetch data when component is mounted

--- step-capstone/src/App.js
+++ step-capstone/src/App.js
@@ -3,10 +3,43 @@ import './styles/App.css';
 import TravelObject from './components/TravelObject'
 
 class App extends React.Component {
-  render () {
-    return(
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  fetchData() {
+    return [
+      {
+        finalized: true,
+        type: "flight",
+        departureAirport: "BOS",
+        arrivalAirport: "SFO",
+        departureDate: "4:00pm EST",
+        arrivalDate: "7:00pm PST",
+        description: "Additional notes"
+      }
+    ]
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: this.fetchData()
+    })
+  }
+
+  render() {
+    if (this.state.data.length === 0) {
+      return null;
+    }
+    return (
       <div className="App">
-        <TravelObject type="flight" />
+
+        <TravelObject
+          data={this.state.data[0]}
+        />
       </div>
     );
   }
