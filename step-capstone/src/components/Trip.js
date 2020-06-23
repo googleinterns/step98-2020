9757d00e7b609d80791b7d1f570640dacf37d559
Emmie Ohnuki <emaohn@gmail.com>
2020-06-23 14:00:04 -0400

    Position map, itinerary, and unordered list relative to one another

--- step-capstone/src/components/Trip.js
+++ step-capstone/src/components/Trip.js
@@ -1,49 +1,55 @@
 import React from 'react';
-import './styles/App.css';
-import TravelObject from './components/TravelObject'
+import TravelObject from './TravelObject'
 import { Box, Grid, Typography } from '@material-ui/core'
 
 export default class Trip extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          data: []
+            data: []
         }
-      }
-    
-      fetchData() {
+    }
+
+    fetchData() {
         return [
-          {
-            finalized: true,
-            type: "flight",
-            departureAirport: "BOS",
-            arrivalAirport: "SFO",
-            departureDate: "4:00pm EST",
-            arrivalDate: "7:00pm PST",
-            description: "Additional notes"
-          }
+            {
+                finalized: true,
+                type: "flight",
+                departureAirport: "BOS",
+                arrivalAirport: "SFO",
+                departureDate: "4:00pm EST",
+                arrivalDate: "7:00pm PST",
+                description: "Additional notes"
+            }
         ]
-      }
-    
-      componentDidMount() {
+    }
+
+    componentDidMount() {
         this.setState({
-          data: this.fetchData()
+            data: this.fetchData()
         })
-      }
-      
-      render() {
+    }
+
+    render() {
         if (this.state.data.length === 0) {
-          return null;
+            return null;
         }
         return (
-          <div className="App">
-            <div id="map-component"></div>
-            <div id="itinerary-component"></div>
-            <div id="unordered-objects-component"></div>
-            <TravelObject
-              data={this.state.data[0]}
-            />
-          </div>
+            <div className="App">
+                <Grid id="map-component">hi</Grid>
+                <Grid container className="foreground" direction="row" justify="space-between">
+                    <Grid item id="itinerary-component">
+                        <TravelObject
+                            data={this.state.data[0]}
+                        />
+                    </Grid>
+                    <Grid item id="unordered-objects-component">
+                        <TravelObject
+                            data={this.state.data[0]}
+                        />
+                    </Grid>
+                </Grid>
+            </div>
         );
-      }
+    }
 }
