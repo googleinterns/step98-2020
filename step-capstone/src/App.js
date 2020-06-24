8fb21b545173e211d04f4419e71549d93ede68ac
Zach Irving <zirving@lion.lmu.edu>
2020-06-24 16:07:54 -0400

    app skeleton

--- step-capstone/src/App.js
+++ step-capstone/src/App.js
@@ -1,15 +1,60 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import './styles/App.css';
-import TravelObject from './components/TravelObject'
 
 class App extends React.Component {
+  isLoggedIn = true; 
+  handleLogin(){
+    if(this.isLoggedIn){
+      return(
+        <Redirect to = "/trip-list"/>
+      ); 
+    } else {
+      return (
+        <Redirect to = "/login-page"/>
+      );
+    }
+  }
   render () {
     return(
       <div className="App">
-        <TravelObject type="flight" />
+        <Router>
+          {this.handleLogin()}
+          <Switch>
+            <Route path="/trip-list">
+              <TripList userId = 'userId' />
+            </Route>
+            <Route path="/login-page">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      
       </div>
     );
   }
+}
+function Login(){
+  return ( 
+    <Login/>
+    //Login Page
+  );
+}
+
+function TripList(){
+  // const trips = firestore.getTrips(props.userId);
+  // const tripList = trips.map((trip) =>
+  //   <li><TripBox {...trip}/></li>
+  // );
+  return (
+    <h2>Hello World</h2>
+   // <ul> {tripList} </ul>
+  );
 }
 
 export default App;
