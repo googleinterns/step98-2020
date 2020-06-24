700bee86b36cd78b30b2e448b324a89bb9bab35a
Emmie Ohnuki <emaohn@gmail.com>
2020-06-24 12:51:12 -0400

    Add components for event form

--- step-capstone/src/components/AddForm.js
+++ step-capstone/src/components/AddForm.js
@@ -1,6 +1,23 @@
 import React from 'react'
-import { Typography, Card, CardActions, CardContent, Button, TextField } from '@material-ui/core';
-import { Paper, Box, Tabs, Tab } from '@material-ui/core'
+import { useState } from 'react'
+import {
+    Typography,
+    Card,
+    CardActions,
+    CardContent,
+    Button,
+    TextField,
+    Paper,
+    Grid,
+    Tabs,
+    Tab
+} from '@material-ui/core';
+import {
+    MuiPickersUtilsProvider,
+    DateTimePicker
+} from '@material-ui/pickers';
+import DateFnsUtils from "@date-io/date-fns";
+
 
 export default class AddForm extends React.Component {
     constructor(props) {
@@ -22,7 +39,6 @@ export default class AddForm extends React.Component {
     }
 
     render() {
-        console.log("rendering tab")
         return (
             <Card id="add-form-container">
                 <CardContent>
@@ -49,9 +65,28 @@ export default class AddForm extends React.Component {
 }
 
 function AddEvent(props) {
+    const [selectedDate, handleDateChange] = useState(new Date());
 
     return (
-        <TextField id="title" label="Add Title" />
+        <Grid container direction="column">
+            <Grid item>
+                <TextField id="title" label="Add Title" fullWidth/>
+            </Grid>
+            <Grid item container direction="row" justify="space-between">
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+                </MuiPickersUtilsProvider>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+                </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid item>
+                <TextField id="location" label="Add Location" fullWidth/>
+            </Grid>
+            <Grid item>
+                <TextField id="description" label="Add Description" fullWidth multiline/>
+            </Grid>
+        </Grid>
     )
 }
 
