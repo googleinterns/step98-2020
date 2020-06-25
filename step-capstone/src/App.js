198c061978be965e57bd0348a8b7001b3818c315
Dan Le <hieudan225@gmail.com>
2020-06-25 17:35:53 -0400

    Delete debugging console log

--- step-capstone/src/App.js
+++ step-capstone/src/App.js
@@ -43,9 +43,8 @@ class App extends React.Component {
 
   }
   render () {
-    console.log(this.state);
+    
     const display = (this.state.authState)? "block" : "none";
-    console.log(display);
     const displayObject = {display: display};
     return (
       <div>
