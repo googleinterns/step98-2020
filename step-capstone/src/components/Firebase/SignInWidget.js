7ee8e23ec9f1f4509062b7e70571a1c698e2bb03
Dan Le <hieudan225@gmail.com>
2020-06-25 21:52:36 -0400

    Put brand and logo into SignInWidget

--- step-capstone/src/components/Firebase/SignInWidget.js
+++ step-capstone/src/components/Firebase/SignInWidget.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FirebaseContext} from '.';
+import logo from './images/logo.png';
 
 export default class SignInWidget extends React.Component {
     static contextType = FirebaseContext;
@@ -9,6 +10,10 @@ export default class SignInWidget extends React.Component {
     render() {
         return (
             <div>
+                <a className="brand-logo">
+                    <img src={logo} id="logo"></img>
+                    <h1>Welcome to Zed - The Smart Travel Assistant</h1>
+                </a>
                 <div id="firebaseui-auth-container"></div>
             </div>
         )
