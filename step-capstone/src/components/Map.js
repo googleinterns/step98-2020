5841c48ffd9cfc97f4c2d8f1aee4be875a18fd2a
Emmie Ohnuki <emaohn@gmail.com>
2020-06-29 20:02:12 -0400

    Add markers on map and adjust window size based on all markers

--- step-capstone/src/components/Map.js
+++ step-capstone/src/components/Map.js
@@ -8,8 +8,25 @@ import React, { createRef } from 'react'
 
 
 class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coordinates: [
+        {
+          lat: 51.511645,
+          lng: -0.131944
+        },
+        {
+          lat: 51.509963,
+          lng: -0.129336
+        }
+      ]
+    }
+    this.googleMapRef = createRef();
+  }
   
   componentDidMount() {
+    console.log("mounting")
     const loadGoogleMapScript = document.createElement('script');
     loadGoogleMapScript.src =
      'https://maps.googleapis.com/maps/api/js?key='+process.env.REACT_APP_API_KEY+'&libraries=place';
@@ -17,16 +34,31 @@ class Map extends React.Component {
     window.document.body.appendChild(loadGoogleMapScript);
 
     loadGoogleMapScript.addEventListener('load', () => {
-      this.googleMap = this.createMap();
+      var googleMap = this.createMap();
+      let bounds = this.addMarkers(googleMap)
+      googleMap.fitBounds(bounds);
     });
   }
 
-  googleMapRef = createRef();
   createMap() {
-    new window.google.maps.Map(this.googleMapRef.current, {
+    return new window.google.maps.Map(this.googleMapRef.current, {
       zoom: this.props.zoom,
       center: this.props.center,
     })
+  }
+
+  addMarkers(map) {
+    var bounds = new window.google.maps.LatLngBounds();
+    this.state.coordinates.map((coord) => {
+      console.log(coord)
+      new window.google.maps.Marker({
+        position: coord,
+        map: map,
+        animation: window.google.maps.Animation.DROP
+      })
+      bounds.extend(coord)
+    })
+    return bounds;
   }
 
   render() {
