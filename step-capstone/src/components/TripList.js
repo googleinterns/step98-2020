1f966e93a2aafd68478fa403befcb27a76027d1b
Zach Irving <zirving@lion.lmu.edu>
2020-07-06 16:45:56 -0400

    initial adding of addTrip button

--- step-capstone/src/components/TripList.js
+++ step-capstone/src/components/TripList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core'
 import TripItemComponent from "./TripItemComponent"
 import {FirebaseContext} from './Firebase';
+import AddItemButton from './AddItemButton';
 import {
   Switch,
   Route,
@@ -15,16 +16,16 @@ class TripList extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      userId : sessionStorage.getItem("userId"),
       selectedTrip : null,
+      reference : "/users/" + sessionStorage.getItem("userId") + "/trips",
       trips : []};
     this.handleOpenTrip = this.handleOpenTrip.bind(this);
+    this.handleAddTrip = this.handleAddTrip.bind(this);
   }
 
   componentDidMount() {
     let trips = [];
-    const reference = "/users/" + this.state.userId + "/trips";
-    this.context.getTripList(reference).then(tripList => {
+    this.context.getTripList(this.state.reference).then(tripList => {
       tripList.forEach(trip => {
         trips.push(trip)
       })
@@ -41,21 +42,38 @@ class TripList extends React.Component{
     sessionStorage.setItem("tripId",tripId);
   }
 
+  handleAddTrip(trip) {
+    this.context.addTrip(this.state.reference, trip)
+    .then(() => {
+      this.setState({trips : this.state.trips.concat(trip)})
+    })
+    .catch(error => {
+      console.log("Error Adding Item")
+      console.error(error)
+    });
+  }
+
   render () {
     const trips = this.state.trips;
     return (
       <div>
         {this.state.selectedTrip ? <Redirect to = {"/trips/"+this.state.selectedTrip}/> :
-          <Grid id="trips" container spacing = {2}> {
-            trips.map((trip) => {
-              return (
-                <Grid item>
-                  <TripItemComponent key={trip.id} data={trip.data()} tripId={trip.id} onOpenTrip={this.handleOpenTrip} />
-                </Grid>
-              );
-            })
-          }
-          </Grid>
+          <div>
+            <Grid id="trips" container spacing = {2}> {
+              trips.map((trip) => {
+                return (
+                  <Grid item>
+                    <TripItemComponent key={trip.id} data={trip.data()} tripId={trip.id} onOpenTrip={this.handleOpenTrip} />
+                  </Grid>
+                );
+              })
+            }
+            </Grid>
+            <Grid id="add-trip-button">
+              <p>Get started on your next advenure</p>
+              <AddItemButton onAddItem={this.handleAddTrip} />
+            </Grid>
+          </div>
         }
       </div>
 
