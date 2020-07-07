5453e396bc5c15a89f03e9585d4a2d4a01681726
Emmie Ohnuki <eohnuki@google.com>
2020-07-06 22:38:54 -0400

    final fixes

--- step-capstone/src/scripts/MapScripts.js
+++ step-capstone/src/scripts/MapScripts.js
@@ -2,7 +2,7 @@ const APIKEY = process.env.REACT_APP_IP_RESTRICTED
 
 const getTimezoneFromCoordinates = (lat, lng) => {
     return new Promise(resolve => {
-        fetch("https://maps.googleapis.com/maps/api/timezone/json?location=" + lat + "," + lng + "&timestamp=1458000000&key=" + APIKEY)
+        fetch("https://maps.googleapis.com/maps/api/timezone/json?location="+lat+","+lng+"&timestamp=1458000000&key="+APIKEY)
             .then(result => result.json())
             .then(data => {
                 resolve(data.timeZoneName);
@@ -14,7 +14,7 @@ const getTimezoneFromCoordinates = (lat, lng) => {
 /* Gets placeID and returns string representation of address and coordinates */
 const getLocation = (placeID) => {
     return new Promise(resolve => {
-        fetch("https://maps.googleapis.com/maps/api/geocode/json?place_id=ChIJd8BlQ2BZwokRAFUEcm_qrcA&key=" + APIKEY)
+        fetch("https://maps.googleapis.com/maps/api/geocode/json?place_id="+placeID+"&key=" + APIKEY)
             .then(result => result.json())
             .then(data => {
                 resolve({address: data.results[0].formatted_address, coordinates: data.results[0].geometry.location});
