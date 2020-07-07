4bfad1082a021190c86bf63790b8c12bf05af786
Zach Irving <zirving@lion.lmu.edu>
2020-07-07 16:57:57 -0400

    finished firebase and sessionStorage mock

--- step-capstone/src/__tests__/TripListTests.test.js
+++ step-capstone/src/__tests__/TripListTests.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import TripList from '../components/TripList';
 import renderer from 'react-test-renderer';
 import {FirebaseContext } from '../components/Firebase';
-import firebaseMock from '../setupTests'
+import firebaseMock from '../components/firebaseMock'
 
 // jest.mock('firebase');
 test('TripList displays correctly', () => {
   sessionStorage.setItem("userId", "testUser");
   const tripListComponent = renderer.create(
-    <FirebaseContext.Provider value={firebaseMock}>
+    <FirebaseContext.Provider value={new firebaseMock()}>
     
       <TripList/>
     </FirebaseContext.Provider>
