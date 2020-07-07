5dbed5ba25973ee4858f7199ad0f4e89d2b02b95
Emmie Ohnuki <eohnuki@google.com>
2020-07-07 13:31:11 -0400

    Autocomplete suggestions input and dropdown

---
+++ step-capstone/src/components/LocationAutocompleteInput.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
+import { TextField } from "@material-ui/core"
+
+export default class LocationAutocompleteInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            place: '',
+            bounds: new window.google.maps.LatLngBounds()
+        };
+    }
+
+    componentDidMount() {
+        this.state.bounds.extend(new window.google.maps.LatLng(-90, -180))
+        this.state.bounds.extend(new window.google.maps.LatLng(90, 180))
+    }
+
+    handleChange = place => {
+        this.setState({ place });
+    };
+
+    handleSelect = place => {
+        console.log(place)
+        geocodeByAddress(place)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => console.log('Success', {address: place, coordinates: latLng}))
+            .catch(error => console.error('Error', error));
+    };
+
+    renderSuggestions({ getInputProps, suggestions, getSuggestionItemProps, loading }) {
+        return (
+            <div>
+                <TextField
+                    {...getInputProps({
+                        placeholder: 'Ex. London Bridge',
+                        className: 'location-search-input',
+                    })}
+                />
+                <div className="autocomplete-dropdown-container">
+                    {loading && <div>Loading...</div>}
+                    {suggestions.map(suggestion => {
+                        // highlights suggestion if being hovered over
+                        const style = suggestion.active
+                            ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                            : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                        return (
+                            <div
+                                {...getSuggestionItemProps(suggestion, {
+                                    style,
+                                })}
+                            >
+                                <span>{suggestion.description}</span>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <PlacesAutocomplete
+                value={this.state.place}
+                onChange={this.handleChange}
+                onSelect={this.handleSelect}
+                searchOptions={{
+                    bounds: this.state.bounds
+                }}
+            >
+                {this.renderSuggestions}
+            </PlacesAutocomplete>
+        );
+    }
+}
