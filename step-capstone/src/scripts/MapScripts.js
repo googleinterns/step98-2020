b10dd2cb04a3b9451d1edd44c33f417f138bf9c0
Emmie Ohnuki <eohnuki@google.com>
2020-07-07 17:01:31 -0400

    Remove coordinate conversion function

--- step-capstone/src/scripts/MapScripts.js
+++ step-capstone/src/scripts/MapScripts.js
@@ -5,23 +5,12 @@ const getTimezoneFromCoordinates = (lat, lng) => {
         fetch("https://maps.googleapis.com/maps/api/timezone/json?location="+lat+","+lng+"&timestamp=1458000000&key="+APIKEY)
             .then(result => result.json())
             .then(data => {
+                console.log(data)
                 resolve(data.timeZoneName);
             });
     });
 
 }
 
-/* Gets placeID and returns string representation of address and coordinates */
-const getLocationByPlaceID = (placeID) => {
-    return new Promise(resolve => {
-        fetch("https://maps.googleapis.com/maps/api/geocode/json?place_id="+placeID+"&key=" + APIKEY)
-            .then(result => result.json())
-            .then(data => {
-                resolve({address: data.results[0].formatted_address, coordinates: data.results[0].geometry.location});
-            });
-    });
-}
-
-
-export default { getTimezoneFromCoordinates, getLocationByPlaceID}
+export default { getTimezoneFromCoordinates }
 
