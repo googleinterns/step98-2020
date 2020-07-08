68256aeece3691eb264734a6064e59e84da8f808
Emmie Ohnuki <eohnuki@google.com>
2020-07-08 14:43:13 -0400

    Fix firebase issues

--- step-capstone/src/components/Trip.js
+++ step-capstone/src/components/Trip.js
@@ -4,95 +4,119 @@ import Unfinalized from './Unfinalized';
 import AddItemButton from './AddItemButton'
 import { Grid } from '@material-ui/core'
 import '../styles/Trip.css'
-import {FirebaseContext} from './Firebase';
+import { FirebaseContext } from './Firebase';
 import MapComponent from "./Map"
 
 export default class Trip extends React.Component {
-  static contextType = FirebaseContext;
+    static contextType = FirebaseContext;
     constructor(props) {
         super(props);
 
         this.state = {
-            reference :"/users/" + sessionStorage.getItem("userId") +"/trips/" + sessionStorage.getItem("tripId"),
-            items: []
+            reference: "/users/" + sessionStorage.getItem("userId") + "/trips/" + sessionStorage.getItem("tripId"),
+            items: [],
+            tripSetting: {
+                startDate: new Date(),
+                endDate: new Date(),
+                destinations: []
+            }
         }
+
+        this.handleRemoveItem = this.handleRemoveItem.bind(this);
+        this.handleEditItem = this.handleEditItem.bind(this);
+        this.handleAddItem = this.handleAddItem.bind(this);
     }
+
     componentDidMount() {
-      let travelObjectList = [];
-      this.context.getTrip(this.state.reference)
-      .then(data => {
-        data.data().travelObjects.forEach(travelObject => {
-          travelObjectList.push(travelObject)
-        });
-        this.setState({items : travelObjectList});
-      })
-      .catch(error => {
-        console.log("Error Getting Trip Data")
-        console.error(error)
-      });
+        let travelObjectList = [];
+        this.context.getTrip(this.state.reference)
+            .then(data => {
+                data.data().travelObjects.forEach(travelObject => {
+                    console.log(travelObject)
+                    travelObject.startDate = travelObject.startDate.toDate();
+                    travelObject.endDate = travelObject.endDate.toDate();
+                    travelObjectList.push(travelObject)
+                });
+                this.setState({ items: travelObjectList });
+            })
+            .catch(error => {
+                console.log("Error retrieving trip data");
+                console.error(error);
+            });
     }
 
     handleRemoveItem(data) {
-      this.context.deleteTravelObject(this.state.reference, data)
-      .then(() => {
-        this.setState({
-          items: this.state.items.filter((item) => item.id !== data.id)
-        });
-      })
-      .catch(error => {
-        console.log("Error Removing Item")
-        console.error(error)
-      });
+        this.context.deleteTravelObject(this.state.reference, data)
+            .then(() => {
+                this.setState({
+                    items: this.state.items.filter((item) => item.id !== data.id)
+                });
+            })
+            .catch(error => {
+                console.log("Error Removing Item")
+                console.error(error)
+            });
     }
 
     handleEditItem(data) {
-      let newItems = [];
-      let itemToChange;
-      this.state.items.forEach((item) => {
-        if(item.id === data.id) {
-            itemToChange = item;
-            newItems.push(data);
-        } else {
-          newItems.push(item);
-        }
-      });
-      this.context.editTravelObject(this.state.reference, itemToChange, data)
-          .then(() => {
-            this.setState ({items : newItems});
-          })
-          .catch((error) => {
-            console.log("Error Editing Item");
-            console.log(error);
-          });
-      console.log(newItems.length);
-      this.setState({items : newItems});
-      console.log(this.state.items);
+        let newItems = [];
+        let itemToChange;
+        this.state.items.forEach((item) => {
+            if (item.id === data.id) {
+                itemToChange = item;
+                newItems.push(data);
+            } else {
+                newItems.push(item);
+            }
+        });
+        this.context.editTravelObject(this.state.reference, itemToChange, data)
+            .then(() => {
+                this.setState({
+                    items: newItems,
+                    loaded: true
+                });
+            })
+            .catch((error) => {
+                console.log("Error Editing Item");
+                console.log(error);
+            });
+        console.log(newItems.length);
+        this.setState({ items: newItems });
+        console.log(this.state.items);
     }
 
     handleAddItem(data) {
-      // Add to database here
-      data.id = Date.now();
-      this.context.addTravelObject(this.state.reference, data)
-      .then(() => {
-        this.setState({items : this.state.items.concat(data)});
-      })
-      .catch(error => {
-        console.log("Error Adding Item")
-        console.error(error)
-      });
+        // Add to database here
+        data.id = Date.now();
+        this.context.addTravelObject(this.state.reference, data)
+            .then(() => {
+                this.setState({ items: this.state.items.concat(data) });
+            })
+            .catch(error => {
+                console.log("Error Adding Item")
+                console.error(error)
+            });
     }
-    
-
     handleEditTripSetting(newSetting) {
         this.setState({
             tripSetting: newSetting
         });
     }
-    
+
     render() {
+        if (this.props.items === undefined) {
+            // TODO: redirect to Home page
+        }
         return (
             <div className="trip">
-                <Grid id="map-component"><Map zoom={13} center={{ lat: 51.5, lng: 0.087 }} /></Grid>
+                <Grid id="map-component">
+                    <MapComponent
+                        zoom={13}
+                        center={{ lat: 51.5, lng: 0.087 }}
+                        finalized={this.state.items.filter((item) => item.finalized)}
+                        unfinalized={this.state.items.filter((item) => !item.finalized && item.coordinates !== undefined)}
+                    />
+                </Grid>
                 <Grid container className="foreground" direction="row" justify="space-between">
                     <Grid item id="finalized-component">
                         <Finalized
@@ -110,23 +134,18 @@ export default class Trip extends React.Component {
                             onRemoveItem={this.handleRemoveItem}
                             onEditItem={this.handleEditItem}
                             onAddItem={this.handleAddItem}
-                            tripSetting= {this.state.tripSetting}
-                            onEditTripSetting = {this.handleEditTripSetting}
+                            tripSetting={this.state.tripSetting}
+                            onEditTripSetting={this.handleEditTripSetting}
                         />
                     </Grid>
                 </Grid>
                 <Grid id="add-button-component">
-                    <AddItemButton 
-                      startDate={this.state.tripSetting.startDate}
-                      onAddItem={this.handleAddItem} />
+                    <AddItemButton
+                        startDate={this.state.tripSetting.startDate}
+                        onAddItem={this.handleAddItem}
+                    />
                 </Grid>
             </div>
         );
     }
 }
-
-
-  
- 
- 
- 
