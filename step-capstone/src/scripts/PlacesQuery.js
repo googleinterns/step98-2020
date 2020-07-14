cc0e6c3d8cf4ac794c1740d8988c9631148ebde2
Dan Le <hieudan225@gmail.com>
2020-07-14 10:05:14 -0400

    Remove rebundant code:

--- step-capstone/src/scripts/PlacesQuery.js
+++ step-capstone/src/scripts/PlacesQuery.js
@@ -1,6 +1,5 @@
 export default function queryPlacesByType (coordinates, radius, service, type) {
     var output = [];
-    var page = 0;
 
     var place = new window.google.maps.LatLng(coordinates.lat, coordinates.lng);
     var request = {
@@ -10,13 +9,11 @@ export default function queryPlacesByType (coordinates, radius, service, type) {
     };
     
     return new Promise((res) => {
-        var getNextPage = null;
         service.nearbySearch(request, function (results, status, pagination) {
             if (status === window.google.maps.places.PlacesServiceStatus.OK) {
                 output = output.concat(results);
-                page = page + 1;
                 console.log("type ", type);
-                if (pagination.hasNextPage && page < 3) {
+                if (pagination.hasNextPage) {
                     console.log("getting next page")
                     pagination.nextPage();
                 }
