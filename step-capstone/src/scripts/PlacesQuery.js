d35a7ed24d7c1bb3bfbaa7e2cdb19205e9ea90a7
Dan Le <hieudan225@gmail.com>
2020-07-14 09:53:32 -0400

    Remove redundant code

--- step-capstone/src/scripts/PlacesQuery.js
+++ step-capstone/src/scripts/PlacesQuery.js
@@ -1,24 +1,27 @@
 export default function queryPlacesByType (coordinates, radius, service, type) {
-    let dict = {"results": [], "natural_feature": 0, "tourist_attraction": 0}
-    var place = new window.google.maps.LatLng(coordinates.lat, coordinates.lng)
+    var output = [];
+    var page = 0;
+
+    var place = new window.google.maps.LatLng(coordinates.lat, coordinates.lng);
     var request = {
         location: place,
         radius: radius,
         types: [type]
     };
+    
     return new Promise((res) => {
         var getNextPage = null;
         service.nearbySearch(request, function (results, status, pagination) {
             if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-                dict["results"] = dict["results"].concat(results);
-                dict[type] = dict[type] + 1;
+                output = output.concat(results);
+                page = page + 1;
                 console.log("type ", type);
-                if (pagination.hasNextPage && dict[type] < 3) {
+                if (pagination.hasNextPage && page < 3) {
                     console.log("getting next page")
                     pagination.nextPage();
                 }
                 else {
-                    res(new Set(dict["results"]));
+                    res(new Set(output));
                 }
                 
             }
