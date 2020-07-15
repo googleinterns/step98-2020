3f98629fe857453bd7b4d1f450b32535ff81c3ee
Zach Irving <zirving@lion.lmu.edu>
2020-07-15 18:40:21 -0400

    added tests for suggestions

---
+++ step-capstone/src/testData/SuggestionTestData.js
@@ -0,0 +1,127 @@
+export const suggestionTestData = {
+  prefLowBudgetFamilyEnt: {
+    userCat: ["familyFriendly", "entertainment"],
+    userBudget: 1
+  }
+
+  , prefMidBudgetFamilyEnt: {
+    userCat: ["familyFriendly", "entertainment"],
+    userBudget: 2
+  }
+
+  , prefHighBudgetFamilyEnt: {
+    userCat: ["familyFriendly", "entertainment"],
+    userBudget: 3
+  }
+
+  , aquarium: {
+    place: {
+      place_id: 0,
+      types: ["tourist_attraction", "aquarium"],
+      price_level: 0,
+      rating: 4
+    },
+    prominence: {
+      index: 5,
+      total: 10
+    },
+    
+  }
+
+  , prominentAquarium: {
+    place: {
+      place_id: 4,
+      types: ["tourist_attraction", "aquarium"],
+      price_level: 0,
+      rating: 4
+    },
+    prominence: {
+      index: 1,
+      total: 10
+    },
+
+  }
+
+  , museum: {
+    place: {
+      place_id: 5,
+      types: ["tourist_attraction", "museum"],
+      price_level: 2,
+      rating: 4
+    },
+    prominence: {
+      index: 5,
+      total: 10
+    },
+
+  }
+
+  , expensiveMuseum: {
+    place: {
+      place_id: 1,
+      types: ["tourist_attraction", "museum"],
+      price_level: 3,
+      rating: 4.1
+    },
+    prominence: {
+      index: 5,
+      total: 10
+    },
+
+  } 
+
+  , expensiveZoo: {
+    place: {
+      place_id: 7,
+      types: ["tourist_attraction", "zoo"],
+      price_level: 3,
+      rating: 4.1
+    },
+    prominence: {
+      index: 5,
+      total: 10
+    },
+
+  }
+
+  , mountain: {
+    place: {
+      place_id: 2,
+      types: ["naturalFeature"],
+      rating: 4
+    },
+    prominence: {
+      index: 5,
+      total: 10
+    },
+
+  }
+
+  , highRatedMountain: {
+    place: {
+      place_id: 6,
+      types: ["naturalFeature"],
+      rating: 5
+    },
+    prominence: {
+      index: 5,
+      total: 10
+    },
+
+  }
+
+
+  , lodging: {
+    place: {
+      place_id: 3,
+      types: ["naturalFeature", "lodging"],
+      price_level: 2,
+      rating: 4
+    },
+    prominence: {
+      index: 5,
+      total: 10
+    },
+
+  }
+}
