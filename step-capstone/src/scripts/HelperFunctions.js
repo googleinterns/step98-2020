d8cbe5a2c738f4ce516c7cc51fd5230eef500b1e
Dan Le <hieudan225@gmail.com>
2020-07-16 21:55:45 -0400

    Add sameDate function to helper function

--- step-capstone/src/scripts/HelperFunctions.js
+++ step-capstone/src/scripts/HelperFunctions.js
@@ -9,3 +9,8 @@ export const travelObjectStartDateComparator = (travelObjectA, travelObjectB) =>
       return -1;
     }
 }
+
+/*Given 2 Date objects, return true if they have the same date; return false otherwise */
+export const sameDate = (timeA, timeB) => {
+  return (timeA.getDate() === timeB.getDate() && timeA.getMonth() === timeB.getMonth() && timeA.getFullYear() === timeB.getFullYear());
+}
