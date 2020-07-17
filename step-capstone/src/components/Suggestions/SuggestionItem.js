d15112962e34ed99738bc8b4e1e59cdf96d7fb17
Zach Irving <zirving@lion.lmu.edu>
2020-07-17 14:28:00 -0400

    suggestion bar

---
+++ step-capstone/src/components/Suggestions/SuggestionItem.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  Typography
+} from '@material-ui/core'
+import FormPopover from "../TravelObjectForms/FormPopover"
+import "../../styles/Suggestions.css"
+import logo from "../../images/logo.png"
+
+export default function SuggestionItem(props) {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+
+
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const printDollarSigns = (budget) => {
+    if (budget === undefined) {
+      return "";
+    } else if (budget === 0) {
+      return "Free";
+    } else {
+      let cost = "";
+      for (let i = 0; i < budget; i++) {
+        cost += "$";
+      }
+      return cost;
+    }
+  }
+
+  const suggestionToData = (suggestion) => {
+    return {
+      title: (props.suggestion.place.name === undefined) ? "" : props.suggestion.place.name,
+      placeId: props.suggestion.place_id,
+      finalized: props.context.finalized,
+      startDate: props.context.startDate,
+      endDate: props.context.endDate,
+      location: props.suggestion.place.vicinity,
+      coordinates: {
+        lat: props.suggestion.place.geometry.location.lat(),
+        lng: props.suggestion.place.geometry.location.lng()
+      } ,
+      description :"",
+      type: "event"
+    }
+  }
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <Card className="suggestion-item" >
+      <CardHeader titleTypographyProps={{noWrap:"true"}} title={(props.suggestion.place.name === undefined) ? "" : props.suggestion.place.name} />
+
+      <CardMedia
+        className="suggestion-photo"
+        component="img"
+        image={(props.suggestion.place.photos === undefined) ? logo : props.suggestion.place.photos[0].getUrl()}
+        title="suggestion photo"
+      />
+
+      <CardContent>
+        <Grid container justify="space-between">  
+          <Typography className="suggestion-body" variant="subtitle2" inline gutterBottom align="left">
+            Price: {printDollarSigns(props.suggestion.place.price_level)} 
+          </Typography>
+          <Typography className="suggestion-body" variant="subtitle2" inline gutterBottom align="right">
+            Rating: {(props.suggestion.place.rating === undefined) ? "" : props.suggestion.place.rating}
+          </Typography>
+        </Grid>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="contained" onClick={handleClick}>
+          Add
+          </Button>
+      </CardActions>
+
+
+      <FormPopover
+        data={suggestionToData(props.suggestion)}
+        isNewItem={true}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onAddItem={props.onAddItem}
+      />
+    </Card>
+  )
+}
