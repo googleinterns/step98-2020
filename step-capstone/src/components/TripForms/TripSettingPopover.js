da902a01887ca5defc2c97d6c000cbe24df5eb74
Emmie Ohnuki <eohnuki@google.com>
2020-07-16 23:19:58 -0400

    Fix indentation

--- step-capstone/src/components/TripForms/TripSettingPopover.js
+++ step-capstone/src/components/TripForms/TripSettingPopover.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Backdrop, Button, makeStyles, Popover} from '@material-ui/core';
+import { Backdrop, Button, makeStyles, Popover } from '@material-ui/core';
 import TripSettingFormPopover from './TripSettingFormPopover';
 import AddIcon from '@material-ui/icons/Add';
 
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function TripSettingPopover(props) {
   const classes = useStyles();
-  
+
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [openBackDrop, setOpenBackDrop] = React.useState(false);
 
@@ -29,38 +29,40 @@ export default function TripSettingPopover(props) {
   const open = Boolean(anchorEl);
   return (
     <div>
-      {(props.button) ? <Button variant='outlined'onClick={handleClick} >Trip setting</Button> : null}
-      {(!props.button)? <AddIcon 
-                style={{position:"relative",
-                        fontSize:"100", 
-                        justify:"center", 
-                        left:"110px", 
-                        top:"150px"}}
-                onClick={handleClick}           
-            /> : null}
+      {(props.button) ? <Button variant='outlined' onClick={handleClick} >Trip setting</Button> : null}
+      {(!props.button) ? <AddIcon
+        style={{
+          position: "relative",
+          fontSize: "100",
+          justify: "center",
+          left: "110px",
+          top: "150px"
+        }}
+        onClick={handleClick}
+      /> : null}
 
       <Backdrop className={classes.backdrop} open={openBackDrop}></Backdrop>
       <Popover
-              open={open}
-              anchorEl={anchorEl}
-              onClose={handleClose}
-              anchorReference="anchorPosition"
-              anchorPosition={{ top: 200, left: 450 }}
-              anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-              }}
-              transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-              }}
-          >
-              <TripSettingFormPopover
-                  onClose={handleClose} 
-                  tripSetting= {props.tripSetting}
-                  onEditTripSetting = {props.onEditTripSetting}
-              />
-          </Popover>
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 200, left: 450 }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <TripSettingFormPopover
+          onClose={handleClose}
+          tripSetting={props.tripSetting}
+          onEditTripSetting={props.onEditTripSetting}
+        />
+      </Popover>
     </div>
   );
 }
