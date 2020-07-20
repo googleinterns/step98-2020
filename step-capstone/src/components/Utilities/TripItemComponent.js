192573ba1b1b2d89d3e266fa18bce4f50c4228d4
Zach Irving <zirving@lion.lmu.edu>
2020-07-20 13:34:46 -0400

    console log remove and formatting:

--- step-capstone/src/components/Utilities/TripItemComponent.js
+++ step-capstone/src/components/Utilities/TripItemComponent.js
@@ -1,46 +1,46 @@
 import React from 'react'
 import {
-    Button,
-    Card,
-    CardHeader,
-    CardMedia,
-    CardContent,
-    CardActions,
-    Typography
+  Button,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-    root: {
-        height: 400,
-        maxWidth: 345,
-    },
-    media: {
-        height: 140
-    },
+  root: {
+    height: 400,
+    maxWidth: 345,
+  },
+  media: {
+    height: 140
+  },
 })
 /*
 * Trip component should receive some trip
 */
 export default function TripItemComponent(props) {
-    const classes = useStyles();
-    let dateRange = props.data.startDate.toDate().toDateString() + " - " + props.data.endDate.toDate().toDateString();
-    return(
-          <Card className={classes.root}>
-              <CardHeader title={props.data.title} subheader={dateRange}/>
-              <CardMedia
-                  component="img"
-                  className={classes.media}
-                  src="https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg"
-              />
-              <CardContent>
-                  <Typography variant="body2" gutterBottom>{props.data.description}</Typography>
-              </CardContent>
-              <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small" onClick={() => props.onOpenTrip(props.tripId)}>Open</Button>
-                  <Button size="small" onClick={() => props.onDeleteTrip(props.tripId)}>Delete</Button>
-              </CardActions>
-          </Card>
-    )
+  const classes = useStyles();
+  let dateRange = props.data.startDate.toDate().toDateString() + " - " + props.data.endDate.toDate().toDateString();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title={props.data.title} subheader={dateRange} />
+      <CardMedia
+        component="img"
+        className={classes.media}
+        src="https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg"
+      />
+      <CardContent>
+        <Typography variant="body2" gutterBottom>{props.data.description}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small" onClick={() => props.onOpenTrip(props.tripId)}>Open</Button>
+        <Button size="small" onClick={() => props.onDeleteTrip(props.tripId)}>Delete</Button>
+      </CardActions>
+    </Card>
+  )
 }
