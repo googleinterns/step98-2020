fefabfeaff5f6c4a1287392b461d90bb2f7f47e6
Dan Le <hieudan225@gmail.com>
2020-07-28 10:10:42 -0400

    Create OptimizationForm

--- step-capstone/src/scripts/OptimizationForm.js
+++ step-capstone/src/scripts/OptimizationForm.js
@@ -1,43 +1,59 @@
 import React from "react";
-import { Popover, Typography, Box, Grid, TextField } from "@material-ui/core";
-import { DropzoneArea } from "material-ui-dropzone";
+import { Popper, Typography, Grid } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import DropArea from "./DropArea";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 export default function OptimizationForm(props) {
   return (
-    <div>
-      <Popover
-        open={props.open}
-        onClose={props.onClose}
-        anchorReference="anchorPosition"
-        anchorPosition={{ top: 100, left: 600 }}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "left",
+    <DndProvider backend={HTML5Backend}>
+      <Popper
+        open={Boolean(props.anchorEl)}
+        anchorEl={props.anchorEl}
+        style={{
+          background: "white",
+          overflowY: "scroll",
+          boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)",
         }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        style={{width: "100%", height:"100%"}}
       >
-        <Grid container direction="column">
-          <Grid>
-            <Typography variant={"h3"} gutterBottom>
+        <Grid
+          noWrap
+          container
+          direction="column"
+          className="popper_class"
+          spacing={0}
+          style={{
+            position: "relative",
+            height: "850px",
+            overflowY: "scroll",
+            width: "600px",
+            margin: "0",
+          }}
+        >
+          <Grid item justify="center">
+            <Typography variant={"h4"} gutterBottom>
               Optimization Form
             </Typography>
           </Grid>
-          <Grid item container direction="row" justify="space-between">
-            <Typography variant={"h6"}>Please select the unfinalized items to get optimization</Typography>
+          <Grid
+            item
+            direction="row"
+            alignItems="center"
+            justify="space-between"
+            style={{ justifyContent: "center" }}
+          >
+            <Typography variant={"h6"}>
+              Please select the unfinalized items to get optimization
+            </Typography>
           </Grid>
-          <Grid item container direction="row" justify="space-between"></Grid>
-          <Grid item>
-            <Box
-            style={{width: "100%", height: '100px', borderStyle:"dashed"}}
-            >
-                Drop here
-            </Box>
-          </Grid>
+          <DropArea 
+            onClose={props.onClose}
+            displayDate={props.displayDate}
+            displayItems={props.displayItems}
+          />
         </Grid>
-      </Popover>
-    </div>
+      </Popper>
+    </DndProvider>
   );
 }
