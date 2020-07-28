879beda6dc64825189d669bc17eff1aea2104f8b
Zach Irving <zirving@lion.lmu.edu>
2020-07-28 15:03:41 -0400

    added Edit button to Trip List items

--- step-capstone/src/components/Pages/TripList.js
+++ step-capstone/src/components/Pages/TripList.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Dialog, Grid } from '@material-ui/core';
 import TripItemComponent from "../Utilities/TripItemComponent";
 import AddTrip from '../TripForms/AddTrip';
-import {FirebaseContext} from '../Firebase';
+import { FirebaseContext } from '../Firebase';
 import {
   Redirect
 } from "react-router-dom";
 import { fetchPhoto } from "../../scripts/HelperFunctions"
+import TripSettingFormPopover from "../TripForms/TripSettingFormPopover"
 
 class TripList extends React.Component {
   static contextType = FirebaseContext;
@@ -14,15 +15,21 @@ class TripList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reference : "/users/" + sessionStorage.getItem("userId") + "/trips/",
-      selectedTrip : null,
-      trips : [],
+      reference: "/users/" + sessionStorage.getItem("userId") + "/trips/",
+      selectedTrip: null,
+      trips: [],
+      editingTrip: false,
+      selectedTripSetting: null,
+      selectedTripId: null
     };
     this.map = null;
     this.service = null;
     this.handleOpenTrip = this.handleOpenTrip.bind(this);
     this.handleAddTrip = this.handleAddTrip.bind(this);
     this.handleDeleteTrip = this.handleDeleteTrip.bind(this);
+    this.handleEditTripClick = this.handleEditTripClick.bind(this);
+    this.handleEditTripSetting = this.handleEditTripSetting.bind(this);
+    this.handleCloseTripSetting = this.handleCloseTripSetting.bind(this);
   }
 
   loadTrips() {
@@ -57,8 +64,35 @@ class TripList extends React.Component {
   }
 
   handleDeleteTrip(tripId) {
-    this.context.deleteTrip(this.state.reference+tripId).then(() => this.loadTrips());
+    this.context.deleteTrip(this.state.reference + tripId).then(() => this.loadTrips());
     this.loadTrips();
+  }
+
+  handleEditTripClick(tripId) {
+    this.context.getTrip(this.state.reference + tripId)
+      .then((tripSetting) => {
+        tripSetting = tripSetting.data()
+        tripSetting.startDate = tripSetting.startDate.toDate()
+        tripSetting.endDate = tripSetting.endDate.toDate()
+        this.setState({
+          selectedTripSetting: tripSetting,
+          selectedTripId: tripId,
+          editingTrip: true
+        });
+      })
+  }
+
+  handleEditTripSetting(newSetting) {
+    this.context.editTripSetting(this.state.reference + this.state.selectedTripId, this.state.selectedTripSetting, newSetting)
+      .then(() => { console.log("load trips"); this.loadTrips() })
+  }
+
+  handleCloseTripSetting() {
+    this.setState({
+      editingTrip: false,
+      // selectedTripSetting: null,
+      
+    })
   }
 
   render() {
@@ -66,34 +100,51 @@ class TripList extends React.Component {
     return (
       <div>
         <div id="map"></div>
-        {this.state.selectedTrip ? <Redirect to = {"/trips/"+this.state.selectedTrip}/> :
-          <Grid id="trips" 
-            container spacing = {2} 
-            style={{position: "absolute", 
-                    top: "10%", 
-                    left: "10%"}}
-          > 
-            {trips.map((trip) => {
-              return (
-                <Grid item id="tripItem" key={trip.id}>
-                  <TripItemComponent
-                    key={trip.id}
-                    data={trip.data()}
-                    tripId={trip.id}
-                    onOpenTrip={this.handleOpenTrip}
-                    onDeleteTrip={this.handleDeleteTrip}
-                  />
-                </Grid>
-              );
-            })}
-            <Grid item>
-              <AddTrip
-                onAddTrip = {this.handleAddTrip}/>
-            </Grid>
-          </Grid>
+        {this.state.selectedTrip ? <Redirect to={"/trips/" + this.state.selectedTrip} /> : 
+          <div>
+            <Grid id="trips"
+              container spacing={2}
+              style={{
+                position: "absolute",
+                top: "10%",
+                left: "10%"
+              }}
+            >
+              {trips.map((trip) => {
+                return (
+                  <Grid item id="tripItem" key={trip.id}>
+                    <TripItemComponent
+                      key={trip.id}
+                      data={trip.data()}
+                      tripId={trip.id}
+                      onOpenTrip={this.handleOpenTrip}
+                      onDeleteTrip={this.handleDeleteTrip}
+                      onEditTrip={this.handleEditTripClick}
+                    />
+                  </Grid>
+                );
+              })}
+              <Grid item>
+                <AddTrip
+                  onAddTrip={this.handleAddTrip} />
+              </Grid>
+            </Grid> 
+            
+            <Dialog
+              open={this.state.editingTrip}
+              onClose={this.handleCloseTripSetting}
+              style={{alignItems: "center", justifyContent: "center", display: "flex"}}
+            >
+              <TripSettingFormPopover
+                onClose={this.handleCloseTripSetting}
+                tripSetting={this.state.selectedTripSetting}
+                onEditTripSetting={this.handleEditTripSetting}
+              />
+            </Dialog>
+          </div>
         }
       </div>
-    );
+    )
   }
 }
 
