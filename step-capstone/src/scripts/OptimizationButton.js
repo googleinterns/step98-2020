c59c050706d86f8c3d3b4017b487f5b619d0a312
Dan Le <hieudan225@gmail.com>
2020-07-28 10:10:04 -0400

    Create OptimizationButton

--- step-capstone/src/scripts/OptimizationButton.js
+++ step-capstone/src/scripts/OptimizationButton.js
@@ -1,39 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import CommuteIcon from "@material-ui/icons/Commute";
 import { Fab } from "@material-ui/core";
-import OptimizationForm from "./OptimizationForm";
+import OptimizationForm from './OptimizationForm'
 
 export default function OptimizationButton(props) {
-  // anchor reference point
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleClick = () => {
+    setAnchorEl(window.document.getElementById("optimization-popper"));
   };
 
   const handleClose = () => {
-    setAnchorEl(null);
-    if (props.onClose !== undefined) {
-      props.onClose();
-    }
-  };
+    setAnchorEl(null)
+  }
 
-  const open = Boolean(anchorEl);
   return (
     <div>
       <Fab color="primary" aria-label="optimization" onClick={handleClick}>
         <CommuteIcon />
       </Fab>
-      <OptimizationForm
-        data={props.data}
-        isNewItem={true}
-        startDate={props.startDate}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        style={{width: "500px", height:"500px"}}
-      />
-      
+      {anchorEl ? <OptimizationForm 
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    {...props}
+                    /> : null}
+                    
     </div>
   );
 }
